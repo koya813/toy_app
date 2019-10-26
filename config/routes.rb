c8867a809ed5550c283ca19c1d4a58426ed4942f
tokunagakouya <kouyatokunaga@gmail.com>
2019-10-26 09:01:49 +0900

    静的

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :microposts
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  get  'static_pages/home'
+  get  'static_pages/help'
+  root 'application#hello'
 end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get ..." do
+    get static_pages_..._url
+    assert_response :success
+  end
+
+end
